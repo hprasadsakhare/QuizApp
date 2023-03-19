1bfa7fa19c59eb507322c6824c8a5afc70a8ff91
hprasadsakhare <119033333+hprasadsakhare@users.noreply.github.com>
2023-03-19 10:35:29 +0530

    Add files via upload

---
+++ src/Data/QuizData.js
@@ -0,0 +1,22 @@
+export const QuizData = [
+    {
+        question: "Who coined the term Web3?",
+        options:[ "mark zuckerberg", "satoshi nakamoto", "gavin wood", "vintalik buteri"],
+        answer: 3
+    },
+    {
+        question: "Who first coined the term semantic web?",
+        options:["flona martin", "tim bernerslee", "nova spivack", "scott jackson"],
+        answer: 2
+    },
+    {
+        question: "Which of these is a limitation of web3",
+        options: ["scalability", "cost", "accessibility", "all of the above"],
+        answer: 4
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+];
